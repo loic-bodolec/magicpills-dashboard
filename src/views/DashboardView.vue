5aad129377fe123fb5ada65d1c7c5443b1bd4a5b
loicbdev <loicbodolec@msn.com>
2025-03-20 09:53:22 +0100

    chore: correct typo in TODO comment for style relocation in DashboardView component

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -200,7 +200,7 @@ const tooltips: Record<string, string> = {
 </script>
 
 <style scoped lang="scss">
-// TODO déplacer le style dans dans dashboard.scss
+// TODO déplacer le style dans dashboard.scss
 .v-row {
   display: flex;
   flex-wrap: wrap;
