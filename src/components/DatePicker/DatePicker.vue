5efe262a97955523af229dde20a0519aee078080
loicbdev <loicbodolec@msn.com>
2025-03-20 09:25:48 +0100

    refactor: remove border-radius from DatePicker component for a cleaner design

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -75,7 +75,6 @@ const onDateChange = (value: Date | null) => {
 .v-text-field {
   width: 100%;
   height: 55px;
-  border-radius: 8px;
   font-weight: 500;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
