9afbab9e98017d67881d60921284669ce5889ccc
loicbdev <loicbodolec@msn.com>
2025-03-20 09:27:23 +0100

    refactor: improve comments in DatePicker component for better clarity

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -33,7 +33,6 @@ import { ref, watch, computed } from 'vue';
 import { format } from 'date-fns';
 
 interface Props {
-<<<<<<< HEAD
   modelValue: Date | null; // La valeur de la date sélectionnée (passée par le parent)
   label: string; // Le texte affiché comme étiquette du champ texte
   menu: boolean; // Contrôle l'état du menu (ouvert/fermé)
@@ -48,61 +47,27 @@ const emit = defineEmits(['update:model-value']); // Événement émis pour mett
 const localValue = ref(props.modelValue);
 
 // Surveille les changements de la prop et met à jour l'état local
-=======
-  modelValue: Date | null;
-  label: string;
-  menu: boolean;
-  maxDate?: string;
-  minDate?: string;
-}
-
-const props = defineProps<Props>();
-const emit = defineEmits(['update:model-value']);
-
-// Local state to avoid directly mutating the prop
-const localValue = ref(props.modelValue);
-
-// Watch for changes in the prop and update the local state
->>>>>>> 4012d1c (refacto: create DatePicker component for enhanced date selection functionality)
 watch(() => props.modelValue, (newValue) => {
   localValue.value = newValue;
 });
 
-<<<<<<< HEAD
 // Surveille les changements de l'état local et émet les mises à jour vers le parent
-=======
-// Watch for changes in the local state and emit updates to the parent
->>>>>>> 4012d1c (refacto: create DatePicker component for enhanced date selection functionality)
 watch(localValue, (newValue) => {
   emit('update:model-value', newValue);
 });
 
-<<<<<<< HEAD
 // Gestion de l'état du menu (ouvert/fermé)
 const isMenuOpen = ref(false);
 
 // Formate la date pour l'affichage dans le champ texte (format YYYY-MM-DD)
-=======
-// Gestion de l'état du menu
-const isMenuOpen = ref(false);
-
-// Formate la date pour l'affichage dans le champ texte
->>>>>>> 4012d1c (refacto: create DatePicker component for enhanced date selection functionality)
 const formattedDate = computed(() =>
   props.modelValue ? format(new Date(props.modelValue), 'dd-MM-yyyy') : ''
 );
 
-<<<<<<< HEAD
 // Met à jour la date sélectionnée et ferme le menu
 const onDateChange = (value: Date | null) => {
   emit('update:model-value', value); // Émet la nouvelle date vers le parent
   isMenuOpen.value = false; // Ferme le menu
-=======
-// Met à jour la date et ferme le menu
-const onDateChange = (value: Date | null) => {
-  emit('update:model-value', value);
-  isMenuOpen.value = false;
->>>>>>> 4012d1c (refacto: create DatePicker component for enhanced date selection functionality)
 };
 </script>
 
