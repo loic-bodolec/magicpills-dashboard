2dfa1d4ee4b9dda1ae903d5930051e91aa1f47c2
loicbdev <loicbodolec@msn.com>
2025-03-20 09:47:05 +0100

    refactor: update styling in DatePicker and DashboardView components for improved aesthetics

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -76,6 +76,6 @@ const onDateChange = (value: Date | null) => {
   width: 100%;
   height: 55px;
   font-weight: 500;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 }
 </style>
