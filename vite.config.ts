cfced6756fd641cccb7223142a608ea82bf82982
loicbdev <loicbodolec@msn.com>
2025-04-03 13:55:22 +0200

    refactor: improve Vite configuration by clarifying the createViteConfig function call

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,27 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import vuetify from 'vite-plugin-vuetify';
 import baseUrl from './src/config/baseUrl'; // Importer baseUrl
 
-export default defineConfig({
-  // TODO Utiliser une base dynamique en fonction de l'environnement
-  // base: process.env.NODE_ENV === 'production' ? '/magicpills-dashboard/' : '/',
-  base: '/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+// Fonction pour générer la configuration dynamique
+export function createViteConfig(command: 'serve' | 'build') {
+  return {
+    base: command === 'build' ? '/magicpills-dashboard/' : '/', // Base dynamique
+    plugins: [
+      vue(),
+      vueDevTools(),
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
+}
+
+// Configuration par défaut pour Vite
+export default defineConfig(({ command }) => {
+  // Appelle createViteConfig avec le bon argument
+  return createViteConfig(command);
 });
