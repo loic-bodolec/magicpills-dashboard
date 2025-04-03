9b2587babefd12f0eed77d2201cacd32f1aa8380
loicbdev <loicbodolec@msn.com>
2025-04-03 15:51:58 +0200

    refactor: simplify base URL configuration and update Vite setup for static base

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import vuetify from 'vite-plugin-vuetify';
+import baseUrl from './src/config/baseUrl'; // Importer baseUrl
 
 // Fonction pour générer la configuration dynamique
-export function createViteConfig(command: 'serve' | 'build') {
-  return {
-    base: command === 'build' ? '/magicpills-dashboard/' : '/', // Base dynamique
-    plugins: [
-      vue(),
-      vueDevTools(),
-      vuetify({
-        autoImport: true,
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+export default defineConfig({
+  base: baseUrl, // Utilise la base dynamique importée
+  plugins: [
+    vue(),
+    vueDevTools(),
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  };
-}
-
-// Configuration par défaut pour Vite
-export default defineConfig(({ command }) => {
-  // Appelle createViteConfig avec le bon argument
-  return createViteConfig(command);
+  },
 });
