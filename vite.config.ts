85936d5b75c6a5b3b8443ad92992c58f8d25fc2b
loic-bodolec <91459770+loic-bodolec@users.noreply.github.com>
2025-04-03 09:52:22 +0200

    Dev (#7)
    * refactor: enhance KpiChart component with customizable bar colors and improved styling
    
    * refactor: clean up code structure and improve type definitions in dashboard
    
    * refactor: enhance KpiCard and KpiChart components with improved layout and styling
    
    * feat: init implementation of date filtering in dashboard data retrieval and enhance sales data generation
    
    * refacto: create DatePicker component for enhanced date selection functionality
    
    * refactor: enhance DatePicker component with improved styling and French localization
    
    * refactor: remove border-radius from DatePicker component for a cleaner design
    
    * refactor: improve comments in DatePicker component for better clarity
    
    * refactor: update styling in DatePicker and DashboardView components for improved aesthetics
    
    * refactor: simplify imports and clean up unused code in KpiChart component
    
    * chore: correct typo in TODO comment for style relocation in DashboardView component
    
    * feat: enhance dashboard functionality and documentation for Vue 3 practice
    
    * refactor: add show-adjacent-months prop and improve date formatting in DatePicker component
    
    * chore: update screenshot in README images for better clarity
    
    * feat: add date-fns for date formatting and update DatePicker and dashboard components
    
    * chore: update screenshot in README images for improved clarity
    
    * refactor: remove date formatting from fetchData function in DashboardView
    
    * feat: add GitHub Actions workflow for deployment to GitHub Pages and set base URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  base: '/magicpills-dashboard/', // Set the base URL to match the repository name
   plugins: [
     vue(),
     vueDevTools(),
     vuetify({
-      autoImport: true, // Vuetify avec auto-import des composants
+      autoImport: true,
     }),
   ],
   resolve: {
