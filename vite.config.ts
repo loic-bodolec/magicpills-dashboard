ed5d0d5f07af3a81739ff2b055b562d6764bbbe1
loicbdev <loicbodolec@msn.com>
2025-04-03 10:53:48 +0200

    feat: update Vite config to use dynamic base URL based on mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import vuetify from 'vite-plugin-vuetify';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   // Utiliser une base dynamique en fonction de l'environnement
-  base: process.env.NODE_ENV === 'production' ? '/magicpills-dashboard/' : '/',
+  base: mode === 'production' ? '/magicpills-dashboard/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -20,4 +20,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+}));
